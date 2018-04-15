3177a5a2b65001ee7d2e88430dccf9866d1afdf2
hachibu <raymondsohn@gmail.com>
2018-04-15 11:26:13 -0700

    Add another font

--- docs/assets/javascripts/index.js
+++ docs/assets/javascripts/index.js
@@ -1,33 +1,35 @@
+var colors = [
+  '#0074D9',
+  '#7FDBFF',
+  '#39CCCC',
+  '#3D9970',
+  '#2ECC40',
+  '#FFDC00',
+  '#FF851B',
+  '#FF4136',
+  '#85144b',
+  '#F012BE',
+  '#B10DC9',
+  '#111111',
+  '#AAAAAA',
+  '#DDDDDD'
+];
+var fonts = [
+  "'IBM Plex Mono', monospace",
+  "'Libre Baskerville', serif"
+];
+var font = 1;
+
 function draw() {
   var $sentence = $('.sentence');
   var words = _.chain(window.sentences).sample().split(' ').value();
 
   _.each($sentence.children(), function(child) {
-    $(child).
-      removeClass('fadeIn flipInX').
-      addClass('fadeOut flipOutX');
+    $(child).removeClass('fadeIn flipInX').addClass('fadeOut flipOutX');
   });
 
   _.delay(function() {
     var $words = [];
-    var colors = [
-      '#0074D9',
-      '#7FDBFF',
-      '#39CCCC',
-      '#3D9970',
-      '#2ECC40',
-      '#FFDC00',
-      '#FF851B',
-      '#FF4136',
-      '#85144b',
-      '#F012BE',
-      '#B10DC9',
-      '#111111',
-      '#AAAAAA',
-      '#DDDDDD'
-    ];
-    var fontFamilies = ["'IBM Plex Mono', monospace;"];
-    var fontFamily = _.sample(fontFamilies);
 
     _.each(words, function (word) {
       colors = _.shuffle(colors);
@@ -35,16 +37,17 @@ function draw() {
       var $word = $('<span>').
         addClass('sentence__word glitch').
         attr('data-text', word).
-        css({
-          'background': colors.pop(),
-          'font-family': fontFamily
-        }).
+        css('background', colors.pop()).
         text(word);
 
       $words.push($word);
     });
 
-    $sentence.empty();
+    font = font === 0 ? 1 : 0;
+
+    $sentence.
+      empty().
+      css('font-family', fonts[font]);
 
     var n = 500;
 
